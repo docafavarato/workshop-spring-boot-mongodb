e8a37d32f66b8820ccda7885169ee69966437dae
docafavarato <docafavarato@gmail.com>
2024-03-13 18:16:58 -0300

    User delete with DELETE request

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id); // throw exception
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
