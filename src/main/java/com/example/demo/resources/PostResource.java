e2eaaa001fcf76ec1a74a7099727c46ccf9e8a88
docafavarato <docafavarato@gmail.com>
2024-04-03 12:10:14 -0300

    Simple query with query methods

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Post;
+import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
 
 @RestController
@@ -20,6 +24,13 @@ public class PostResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value="/titlesearch", params={"text"})
+	public ResponseEntity<List<Post>> findByTitleContaining(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> obj = service.findByTitleContaining(text);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitleContaining(String text) {
+		List<Post> obj = repository.findByTitleContainingIgnoreCase(text);
+		return obj;
 	}
 }
